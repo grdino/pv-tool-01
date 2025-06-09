c7bf83d4e1a605ef4231081bc54fc5bfebddd536
grdino <67813644+grdino@users.noreply.github.com>
2025-06-09 00:06:56 -0600

    Update App.js
    changed blue bar verbage to be an h3

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
   return (
     <div className="container">
       <h1>Find Your Ideal Neighborhood in Puerto Vallarta</h1>
-      <label>Slide Blue bar to adjust Price per M2</label>
+      <h3>Slide Blue bar to adjust Price per M2</h3>
       <label>Max Price per m²: ${maxPrice}</label>
       <input
         type="range"
