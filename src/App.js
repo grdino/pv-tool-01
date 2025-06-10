d3010e8724e9e05b0af2007f4e53adc1f86b221e
grdino <67813644+grdino@users.noreply.github.com>
2025-06-10 14:19:21 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const neighborhoods = [
 ];
 
 export default function App() {
-  const [maxPrice, setMaxPrice] = useState(375);
+  const [maxPrice, setMaxPrice] = useState(372);
 
   const filtered = neighborhoods.filter(n => n.price <= maxPrice);
 
