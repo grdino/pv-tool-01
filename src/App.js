fe89d5380bea95bca9d1a100b178bb0e0b12a456
grdino <67813644+grdino@users.noreply.github.com>
2025-06-10 09:44:18 -0600

    Update App.js
    Changed bar max to 4000 from 4500

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const neighborhoods = [
 ];
 
 export default function App() {
-  const [maxPrice, setMaxPrice] = useState(4500);
+  const [maxPrice, setMaxPrice] = useState(4000);
 
   const filtered = neighborhoods.filter(n => n.price <= maxPrice);
 
